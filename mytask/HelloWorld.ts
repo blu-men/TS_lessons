bb487509b8e58ba98ac879c9572b50ec2b059dc9
blu-men <cjbb5eyv@icloud.com>
2021-11-23 20:34:43 +0900

    compiled

---
+++ mytask/HelloWorld.ts
@@ -0,0 +1,5 @@
+function sayHello(name: String): void {
+  console.log(`Hello ${name}!`);
+}
+
+sayHello('Dave');
---
+++ project01/sample04.js
@@ -0,0 +1,5 @@
+"use strict";
+var a = 10;
+var b = 5;
+var ans = a * b;
+console.log(ans);
